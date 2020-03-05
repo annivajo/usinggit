19c8635359629ea0d35f14f8df6bd6c979280664
Ivanova Anna <annivajo@gmail.com>
2020-03-05 12:42:42 +0200

    sout MASTER

--- src/java/main/Main.java
+++ src/java/main/Main.java
@@ -1,10 +1,12 @@
 package main;
 
+
 public class Main {
     public static void main(String[] args) {
         System.out.println("Learning git");
 
         Test t = new Test();
         System.out.println(t.getStr());
+        System.out.println("MASTER");
     }
 }
