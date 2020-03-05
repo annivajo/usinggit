6b1dbddc3858c05406636024b5ad98c718356acc
Ivanova Anna <annivajo@gmail.com>
2020-03-05 12:38:56 +0200

    Add class Test

--- src/java/main/Main.java
+++ src/java/main/Main.java
@@ -4,6 +4,7 @@ public class Main {
     public static void main(String[] args) {
         System.out.println("Learning git");
 
-
+        Test t = new Test();
+        System.out.println(t.getStr());
     }
 }
---
+++ src/java/main/Test.java
@@ -0,0 +1,14 @@
+package main;
+
+public class Test {
+
+    private String str;
+
+    public Test() {
+        str="Hello";
+    }
+
+    public String getStr(){
+        return str;
+    }
+}
